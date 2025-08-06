6b6987eac455ec02bf804efda0311f63fff818b2
Sabrina S. Lima <sanmisabrina@icloud.com>
2025-08-06 17:31:14 -0300

    Delete src directory

--- src/pipe/pipe2.d
+++
@@ -1,6 +0,0 @@
-src/pipe/pipe2.o: src/pipe/pipe2.c include/minishell.h libft/libft.h \
- include/garbage_collector.h include/garbage_collector.h
-include/minishell.h:
-libft/libft.h:
-include/garbage_collector.h:
-include/garbage_collector.h:
